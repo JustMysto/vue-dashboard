b2dd1fce3da0de50a2cb16b170caad799e0ca3f7
JustMysto <markusbiro@yahoo.de>
2021-04-10 04:09:16 +0200

    init files / implement transaction component and fetch api

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
     <img alt="Vue logo" src="./assets/logo.png">
-    <loader/>
-    <transaction/>
+    
+    <div v-if="transactions.length">
+        <div v-for="transaction in transactions" :key="transaction.id">
+            <transaction :person="transaction.person" :projects="transaction.projects" :finished="transaction.finished" :profit="transaction.profit" :followers="transaction.followers"/>
+        </div>
+    </div>
+
+    <div v-else>
+        <loader/>
+    </div>
 </template>
 
 <script>
@@ -11,9 +19,20 @@ import transaction from './components/transaction.vue';
 
 export default {
     name: 'App',
+    data() {
+        return {
+            transactions: []
+        }
+    },
     components: {
         loader,
         transaction
+    },
+    mounted() {
+        fetch('http://localhost:3000/transactions')
+            .then(res => res.json())
+            .then(data => this.transactions = data)
+            .catch(err => console.log(err.message))
     }
 }
 </script>
--- src/components/transaction.vue
+++ src/components/transaction.vue
@@ -1,5 +1,30 @@
 <template>
-    {{person}}
+    <div class="transaction">
+        <div class="person">
+            <img src="../assets/icons/Elements/User/Photo.svg" alt="person">
+            <div class="person-info">
+                <h2 class="person-name">{{ person }}</h2>
+                <h4 class="person-nick">Lima</h4>
+            </div>
+        </div>
+        <div class="transaction-info">
+            <h2 class="transaction-info-heading">{{ projects }}</h2>
+            <h4 class="transaction-info-text">Projects</h4>
+        </div>
+        <div class="transaction-info">
+            <h2 class="transaction-info-heading">{{ finished }}</h2>
+            <h4 class="transaction-info-text">Finished Projects</h4>
+        </div>
+        <div class="transaction-info">
+            <h2 class="transaction-info-heading purple">$ {{ profit }}</h2>
+            <h4 class="transaction-info-text">Total Profit</h4>
+        </div>
+        <div class="transaction-info">
+            <h2 class="transaction-info-heading">{{ followers }}</h2>
+            <h4 class="transaction-info-text">Followers</h4>
+        </div>
+        <img class="arrow" src="../assets/icons/Right.svg" alt="arrow">
+    </div>
 </template>
 
 <script>
@@ -14,6 +39,41 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.transaction {
+    grid-column: span 9;
+    justify-content: space-between;
+    align-items: center;
+    background-color: white;
+    border-radius: 1rem;
+    padding: 1rem;
+    display: grid;
+    grid-template-columns: 1.5fr 1fr 1fr 1fr 1fr 0.2fr;
+    cursor: pointer;
+    box-shadow: 0px 5px 15px -15px black;
+    animation: fadeIn .2s ease-out;
+    transition: background-color .2s;
+}
 
+.transaction:hover {
+    background-color: #F8F8F8;
+}
+
+.person {
+    display: flex;
+    align-items: center;
+    grid-column: 1;
+}
+
+.person-info {
+    margin: 0 0 0 1.5rem;
+}
+
+.transaction-info-text {
+    color: rgba(0, 0, 0, .4);
+}
+
+.person-nick {
+    color: rgba(0, 0, 0, .4);
+}
 </style>
