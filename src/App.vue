4b0107f9434e0fc197dc383ce5d0a8a9abb959d3
JustMysto <markusbiro@yahoo.de>
2021-04-15 00:51:46 +0200

    add transaction-details query handler

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,9 @@
     
     <div v-if="transactions.length">
         <div v-for="transaction in transactions" :key="transaction.id">
-            <transaction :person="transaction.person" :projects="transaction.projects" :finished="transaction.finished" :profit="transaction.profit" :followers="transaction.followers"/>
+            <transaction v-on:click="getTransactionDetails(transaction.id)" :person="transaction.person" :projects="transaction.projects" :finished="transaction.finished" :profit="transaction.profit" :followers="transaction.followers"/>
         </div>
     </div>
-
     <div v-else>
         <loader/>
     </div>
@@ -28,8 +27,17 @@ export default {
         loader,
         transaction
     },
+    methods: {
+        async getTransactionDetails(transactionId) {
+            if (!transactionId) return;
+            await fetch("localhost/transactions/" + transactionId)
+                .then(res => res.json())
+                .then(data => console.log(data))
+                .catch(err => console.log(err.message))
+        }
+    },
     mounted() {
-        fetch('http://localhost:3000/transactions')
+        fetch('http://localhost:3000/transactions/')
             .then(res => res.json())
             .then(data => this.transactions = data)
             .catch(err => console.log(err.message))
