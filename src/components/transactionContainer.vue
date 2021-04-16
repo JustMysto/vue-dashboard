ac6d7e9ef1b5d2d9606831efced512daca92c3c7
Markus Biro <markusbiro@yahoo.de>
2021-04-16 17:10:19 +0200

    upload

---
+++ src/components/transactionContainer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="filter-bar">
+        <input type="text" id="search" placeholder="search">
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
